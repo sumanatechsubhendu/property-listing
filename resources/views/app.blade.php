854e5237362dfee5cd81677a27ae113d536f4cf2
webwizardsusa <webwizardsusa@WWU.local>
2023-12-13 09:43:20 +0530

    install laravel breeze

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,20 +1,20 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
-    <title>Sumanas Listing App</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css" />
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
-    @routes
-    @vite('resources/js/app.js')
-    @inertiaHead
-</head>
-
-<body class="bg-white dark:bg-gray-900 text-gray-800 dark:text-gray-300">
-    @inertia
-</body>
-
+        <!-- Scripts -->
+        @routes
+        @vite('resources/js/app.js')
+        @inertiaHead
+    </head>
+    <body class="font-sans antialiased">
+        @inertia
+    </body>
 </html>
