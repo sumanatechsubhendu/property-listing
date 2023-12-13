b85a47d2af99ae5415c036d7d855bfa9250e2c0a
webwizardsusa <webwizardsusa@WWU.local>
2023-12-13 11:19:34 +0530

    post create form validation is working

--- resources/js/Pages/Post/Create.vue
+++ resources/js/Pages/Post/Create.vue
@@ -12,7 +12,66 @@
             <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
                 <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                     <div class="p-6 bg-white border-b border-gray-200">
-                        <Form :form="form" @submit="createPost" />
+                        <form @submit.prevent="createPost">
+                            <div>
+                                <label for="title">Title</label>
+                                <input type="text" v-model="form.title" @keyup="convertToSlug" class="
+                                    w-full
+                                    px-4
+                                    py-2
+                                    mt-2
+                                    border
+                                    rounded-md
+                                    focus:outline-none
+                                    focus:ring-1
+                                    focus:ring-blue-600
+                                " />
+                                <div v-if="form.errors.title">{{ form.errors.title }}</div>
+                            </div>
+                            <div>
+                                <label for="title">Slug</label>
+                                <input type="text" v-model="form.slug" class="
+                                    w-full
+                                    px-4
+                                    py-2
+                                    mt-2
+                                    border
+                                    rounded-md
+                                    focus:outline-none
+                                    focus:ring-1
+                                    focus:ring-blue-600
+                                " />
+                                <div v-if="form.errors.slug">{{ form.errors.slug }}</div>
+                            </div>
+                            <div class="mt-4">
+                                <label for="title">Description</label>
+                                <textarea name="description" type="text" v-model="form.description" class="
+                                    w-full
+                                    px-4
+                                    py-2
+                                    mt-2
+                                    border
+                                    rounded-md
+                                    focus:outline-none
+                                    focus:ring-1
+                                    focus:ring-blue-600
+                                ">
+            </textarea>
+                                <div v-if="form.errors.description">{{ form.errors.description }}</div>
+                            </div>
+                            <!-- submit -->
+                            <div class="flex items-center mt-4">
+                                <button class="
+                                    px-6
+                                    py-2
+                                    text-white
+                                    bg-gray-900
+                                    rounded
+                                ">
+                                    Save
+                                </button>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
@@ -43,6 +102,13 @@ export default {
     methods: {
         createPost() {
             this.form.post(route("posts.store"));
+        },
+        convertToSlug() {
+            // Convert title to slug
+            this.form.slug = this.form.title
+                .toLowerCase()
+                .replace(/ /g, '-') // Replace spaces with hyphens
+                .replace(/[^\w-]+/g, ''); // Remove non-word characters except hyphens
         },
     },
 };
